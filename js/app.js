dc8e8955841cb09dac56bf30178d0388df50f8db
Lindsey Howard <miss_leading009@yahoo.com>
2022-12-17 04:14:32 -0500

    added some content...

---
+++ js/app.js
@@ -0,0 +1,134 @@
+// Hiding Effect added to each "overlay" effect
+$(".overlay-project-one").hide();
+$(".overlay-project-two").hide();
+$(".overlay-project-three").hide();
+$(".overlay-project-four").hide();
+$(".overlay-project-five").hide();
+$(".overlay-project-six").hide();
+$(".overlay-project-seven").hide();
+$(".overlay-project-eight").show();
+$(".overlay-project-nine").hide();
+$(".overlay-project-ten").hide();
+$(".overlay-project-eleven").hide();
+$(".overlay-project-twelve").hide();
+$(".overlay-project-thirteen").hide();
+
+// Hover Effect for the first project (Photoshop actions)
+$("#project-one").hover(
+	function () {
+		$(".overlay-project-one").slideDown();
+	},
+	function () {
+		$(".overlay-project-one").slideUp();
+	}
+);
+
+// Hover Effect for the second project (nebula backgrounds)
+$("#project-two").hover(
+	function () {
+		$(".overlay-project-two").slideDown();
+	},
+	function () {
+		$(".overlay-project-two").slideUp();
+	}
+);
+
+// Hover Effect for the third project (plexus backgrounds)
+$("#project-three").hover(
+	function () {
+		$(".overlay-project-three").slideDown();
+	},
+	function () {
+		$(".overlay-project-three").slideUp();
+	}
+);
+
+// Hover Effect for the fourt project (2D Planets)
+$("#project-four").hover(
+	function () {
+		$(".overlay-project-four").slideDown();
+	},
+	function () {
+		$(".overlay-project-four").slideUp();
+	}
+);
+
+// Hover Effect for the fifth project (Abstract Shape Backgrounds)
+$("#project-five").hover(
+	function () {
+		$(".overlay-project-five").slideDown();
+	},
+	function () {
+		$(".overlay-project-five").slideUp();
+	}
+);
+
+// Hover Effect for the sixth project (Freebies)
+$("#project-six").hover(
+	function () {
+		$(".overlay-project-six").slideDown();
+	},
+	function () {
+		$(".overlay-project-six").slideUp();
+	}
+);
+
+// Hover Effect for the seventh project (Dark Souls Scene)
+$("#project-seven").hover(
+	function () {
+		$(".overlay-project-seven").slideDown();
+	},
+	function () {
+		$(".overlay-project-seven").slideUp();
+	}
+);
+
+// Hover Effect for the ninth project (Steemit)
+$("#project-nine").hover(
+	function () {
+		$(".overlay-project-nine").slideDown();
+	},
+	function () {
+		$(".overlay-project-nine").slideUp();
+	}
+);
+
+// Hover Effect for the tenth project (Medium)
+$("#project-ten").hover(
+	function () {
+		$(".overlay-project-ten").slideDown();
+	},
+	function () {
+		$(".overlay-project-ten").slideUp();
+	}
+);
+
+// Hover Effect for the eleventh project (Github)
+$("#project-eleven").hover(
+	function () {
+		$(".overlay-project-eleven").slideDown();
+	},
+	function () {
+		$(".overlay-project-eleven").slideUp();
+	}
+);
+
+// Hover Effect for the twelfth project (FreeCodeCamp)
+$("#project-twelve").hover(
+	function () {
+		$(".overlay-project-twelve").slideDown();
+	},
+	function () {
+		$(".overlay-project-twelve").slideUp();
+	}
+);
+
+// Hover Effect for the thirteenth project (CreativeMarket)
+$("#project-thirteen").hover(
+	function () {
+		$(".overlay-project-thirteen").slideDown();
+	},
+	function () {
+		$(".overlay-project-thirteen").slideUp();
+	}
+);
