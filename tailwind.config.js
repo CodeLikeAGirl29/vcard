5769538499a7e33c49b6001eaa12a4c65cb5bdfb
codelikeagirl29 <codelikeagirl91@gmail.com>
2023-10-09 12:55:02 -0500

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-	purge: [],
-	darkMode: false,
-	content: [],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    colors: {
+      gray: '#ABB7B7',
+      blue: '#425cbb',
+      red: '#d81300',
+      pink: '#f62459',
+      yellow: '#ffc500',
+      green: '#15db95'
+    },
+    fontFamily: {
+      sans: ['Eczar', 'serif'],
+      serif: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    }
+  }
+}
